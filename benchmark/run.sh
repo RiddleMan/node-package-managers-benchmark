146549bb140f3f80422b4314ad8e9e86d39707f6
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-18 20:59:54 +0100

    feat: Add test for bootstrap time with node installed

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
 
-# Startup time measurements
-hyperfine --warmup 3 'BASH_ENV="/benchmark/.bashrc.nvm" bash -c "true"' \
-    'BASH_ENV="/benchmark/.bashrc.fnm" bash -c "true"' \
-    'BASH_ENV="/benchmark/.bashrc.asdf" bash -c "true"' \
-    'BASH_ENV="/benchmark/.bashrc.volta" bash -c "true"'
+echo '1) Startup time measurements without Node installed'
+echo 
+
+get_cmd() {
+    tool_name=$1
+    cmd_str=$2
+    printf 'BASH_ENV="/benchmark/.bashrc.%s" bash -c "%s"' "$tool_name" "$cmd_str"
+}
+
+hyperfine --warmup 3 \
+    "$(get_cmd nvm true)" \
+    "$(get_cmd fnm true)" \
+    "$(get_cmd asdf true)" \
+    "$(get_cmd volta true)"
+
+echo 
+echo '2) Startup time measurements with Node installed'
+echo
+
+echo 'Preparing...'
+NODE_LTS_VERSION=16.3.0
+NODE_CURRENT_VERSION=17.1.0
+
+eval "$(get_cmd nvm "nvm install $NODE_LTS_VERSION")"
+eval "$(get_cmd fnm "fnm install $NODE_LTS_VERSION")"
+eval "$(get_cmd asdf "asdf install nodejs $NODE_LTS_VERSION && asdf global nodejs $NODE_LTS_VERSION")"
+eval "$(get_cmd volta "volta install node@${NODE_LTS_VERSION}")"
+
+hyperfine --warmup 3 \
+    "$(get_cmd nvm "node -v")" \
+    "$(get_cmd fnm "node -v")" \
+    "$(get_cmd asdf "node -v")" \
+    "$(get_cmd volta "node -v")"
+
